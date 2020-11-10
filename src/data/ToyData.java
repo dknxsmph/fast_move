61e1ace84c513b51c12b7c88b1d075f5da29fc64
Your Name <immintchyy@gmail.com>
2020-11-10 16:42:53 +0700

    ntk

--- src/data/ToyData.java
+++ src/data/ToyData.java
@@ -2,7 +2,9 @@ package data;
 
 import base.Bike;
 import base.Parcel;
+import base.ParcelStatus;
 import base.Person;
+import base.Location;
 
 public class ToyData {
 
@@ -23,6 +25,16 @@ public class ToyData {
     public static Parcel[] getParcels() {
         Parcel[] parcels = new Parcel[10];
         // your code
+        parcels[0] = new Parcel(5, ParcelStatus.SHIPPING, new Location(50.0, 150.1));
+        parcels[1] = new Parcel(10, ParcelStatus.SHIPPING, new Location(60.0, 250.1));
+        parcels[2] = new Parcel(15, ParcelStatus.SHIPPING, new Location(70.0, 155.1));
+        parcels[3] = new Parcel(20, ParcelStatus.SHIPPING, new Location(80.0, 50.5));
+        parcels[4] = new Parcel(25, ParcelStatus.SHIPPING, new Location(90.0, 50.8));
+        parcels[5] = new Parcel(30, ParcelStatus.SHIPPING, new Location(100.0, 88.1));
+        parcels[6] = new Parcel(35, ParcelStatus.SHIPPING, new Location(110.0, 13.6));
+        parcels[7] = new Parcel(40, ParcelStatus.SHIPPING, new Location(120.0, 20.20));
+        parcels[8] = new Parcel(45, ParcelStatus.SHIPPING, new Location(130.0, 18.1));
+        parcels[9] = new Parcel(50, ParcelStatus.SHIPPING, new Location(140.0, 90.1));    
 
         return parcels;
     }
